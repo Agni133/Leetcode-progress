62a6dd6482564b83eea879eb3f55eef7942e6a76
Agnivesh  Pandita <105123072+Agni133@users.noreply.github.com>
2025-10-13 17:51:17 +0530

    Time: 47 ms (32.79%) | Memory: 53.8 MB (45.86%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function() {
+      return "Hello World"
+               
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
